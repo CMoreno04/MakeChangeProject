188329b80f9b00006196159a7dd5e82f97b93d3d
CMoreno04 <cesar.jos.em26@icloud.com>
2019-09-14 17:31:39 -0600

    Homework Submission

--- src/MakeChangeApp.java
+++ src/MakeChangeApp.java
@@ -1,9 +1,125 @@
+import java.util.Scanner;
 
 public class MakeChangeApp {
+	static Scanner kb;
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		kb = new Scanner(System.in);
+
+		theRegister();
+
+		kb.close();
+	}
+
+	public static void theRegister() {
+		double change = 0, changeHolder = 0, changeDisplay = 0;
+		int pennies = 0, changeDenomination = 0;
+
+		for (int i = 0; i < 5; i++) {
+			System.out.println("\n********** Welcome to \" El Mercado \" Food  Market **********\n");
+			System.out.println("What is the cost of the item?");
+			float itemPrice = kb.nextFloat();
+			System.out.println("Please enter how much did the customer pay? ");
+			float amountPaid = kb.nextFloat();
+
+			change = amountPaid - itemPrice;
+			changeDisplay = change;
+
+			if (amountPaid > itemPrice) {
+
+				System.out.print("\nChange would be ");
+				System.out.printf("%.2f", changeDisplay);
+				System.out.println(" that is: \n");
+
+				if (change >= 20) {
+					changeHolder = change;
+					changeDenomination = (int) change / 20;
+					System.out.println(changeDenomination + " Twenty dollar bill/s.");
+					change = changeHolder - (20 * (changeDenomination));
+
+				}
+
+				if (change >= 10) {
+					changeHolder = change;
+					changeDenomination = (int) change / 10;
+					System.out.println(changeDenomination + " Ten dollar bill/s.");
+					change = changeHolder - (10 * (changeDenomination));
+
+				}
+
+				if (change >= 5) {
+					changeHolder = change;
+					changeDenomination = (int) change / 5;
+					System.out.println(changeDenomination + " Five dollar bill/s.");
+					change = changeHolder - (5 * (changeDenomination));
+
+				}
+
+				if (change >= 1) {
+					changeHolder = change;
+					changeDenomination = (int) change / 1;
+					System.out.println(changeDenomination + " Dollar/s.");
+					change = changeHolder - (1 * (changeDenomination));
+
+				}
+				if (change >= .25) {
+					changeHolder = change;
+					changeDenomination = (int) (change / .25f);
+					System.out.println(changeDenomination + " Quarter/s.");
+					change = changeHolder - (.25 * (changeDenomination));
+
+				}
+
+				if (change >= .10) {
+					changeHolder = change;
+					changeDenomination = (int) (change / .10f);
+					System.out.println(changeDenomination + " Nickel/s.");
+					change = changeHolder - (.10f * (changeDenomination));
+
+				}
+				if (change >= .05) {
+					changeHolder = change;
+					changeDenomination = (int) (change / .05f);
+					System.out.println(changeDenomination + " Dime/s.");
+					change = changeHolder - (.05f * (changeDenomination));
+
+				}
+
+				if (change >= .00) {
+					changeHolder = change;
+					pennies = (int) ((change + .001) / .01);
+					System.out.println(pennies + " Penny/ies.");
+
+				}
+			}
+
+			if (amountPaid == itemPrice) {
+				System.out.println("\nExact Change. Perfect!");
+			}
+
+			if (amountPaid < itemPrice) {
+				System.out.println("\nToo low, please try again.\n ");
+				theRegister();
+			}
+
+			i = stopOrContinue(i);
+		}
+
+		System.out.println("****** Thank you for shopping at El Mercado Food Market, Goodbye! ******");
 
 	}
 
+	public static int stopOrContinue(int i) {
+		String answer = "";
+
+		System.out.println("\nWould you like to purchase another item? Yes / No ");
+		answer = kb.next();
+
+		if (answer.equalsIgnoreCase("y") || answer.equalsIgnoreCase("yes")) {
+			i--;
+		} else if (answer.equalsIgnoreCase("n") || answer.equalsIgnoreCase("no")) {
+			i += 5;
+		}
+		return i;
+	}
 }
--- src/README.md
+++ src/README.md
@@ -1,11 +1,51 @@
-## Make Change Project
+# Make Change Project
 
 ### First week homework for Skill Distillery
 
 ### Overview
+#### In the cash register we will calculate the amount of change returned to a customer based on the purchase price and the amount tendered. We will also notify the attendant how #### many of each piece of currency ($20 ,$10 ,$5 ,$1, .25c, .10c, .05c, .01c) is needed to make the change for the customer. Change should be provided using the largest bill and #### coin denominations as possible. Denominations that are not used should not be displayed.
 
 ### How to run
 
-### Technologies/topics used
+###### ********** Welcome to " El Mercado " Food  Market **********
 
-### Lessons 
+#### 1. The user will input the cost of the item, then press return.  
+##### What is the cost of the item?
+##### .67 #### <-- User's input.
+
+#### 2. The user will input the amount of money tendered, then press return.
+##### Please enter how much did the customer pay?
+##### 1 #### <-- User's input.
+
+#### 3. The owed change will be displayed, followed by the owed amount of bills or/and cents organized by denominations.
+##### Change would be 0.33 that is: #### <-- Change Owed.
+
+#### 4. Change sub-divided in correct denominations.
+##### 1 Quarter/s.
+##### 1 Dime/s.
+##### 3 Penny/ies.
+
+#### 5. The user would have the option to process another item. If Yes or Y is entered, the program restarts and another item can be processed.
+##### Would you like to purchase another item? Yes / No
+##### n
+
+#### On the other hand, if No or N is entered, the goodbye message will appear and the program will terminate.
+##### ****** Thank you for shopping at El Mercado Food Market, Goodbye! ******
+
+
+### Technologies/topics used
+#### Methods
+#### If/else Statements
+#### For Loops
+#### Booleans
+#### Basic Operators
+#### Casting Data Types
+#### Method Calling
+#### Imports
+#### Static Scanner
+#### Multiple Data Types
+
+### Lessons
+#### Decimal number utilization, especially in the .01 and .001 place. When casting double to int, the variable would always round the number to the next lower integer.
+#### This made me realize that for pennies .001 value must be added to the original change in order to eliminate that problem, and the division process went a lot smoother
+#### after this changes where made.
